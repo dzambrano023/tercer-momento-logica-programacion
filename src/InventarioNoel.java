2873328595e591df09db9fdf6a9ecfd2dcf63df4
dzambrano023 <dzambranogr@gmail.com>
2026-09-04 22:11:22 -0500

    Primer ejercicio: Inventario NOEL

---
+++ src/InventarioNoel.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class InventarioNoel {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] productos = {"Ducales", "Festival", "Tosh", null, null};
+        boolean continuar = true;
+        int opcion;
+        System.out.println("********** Bienvenido Inventario **********");
+        while (continuar) {
+            System.out.println("Seleccione la opcion: ");
+            System.out.println("Opcion 1: Listar Producto ");
+            System.out.println("Opcion 2: Buscar Producto ");
+            System.out.println("Opcion 3: Agregar Producto ");
+            System.out.println("Opcion 4: Salir ");
+
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    for (String producto : productos) {
+                        System.out.println("PRODUCTOS REGISTRADOS: " + producto);
+                        if (producto == null) {
+                            System.out.println("         "); // revisar esta linea ??? (Cuando sea null imprima un esacio en blanco)
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("INGRESE EL NOMBRE DEL PRODUCTO: ");
+                    String nombreProducto = sc.next();
+                    buscarProducto(productos, nombreProducto);
+                    if (buscarProducto(productos, nombreProducto)) {
+                        System.out.println("Producto: " + nombreProducto + " Disponible");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Ingrese el producto a Agregar");
+                    agregarProducto(productos, sc.next());
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo...");
+                    continuar = false;
+                    break;
+
+            }
+
+
+        }
+
+    }
+
+    public static boolean buscarProducto(String[] nombreArray, String nombreProducto) {
+
+        for (int i = 0; i < nombreArray.length; i++) {
+
+            if (nombreArray[i].equals(nombreProducto)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void agregarProducto(String[] nombreArray, String nombreProducto) {
+        for (int i = 0; i < nombreArray.length; i++) {
+            if (nombreArray[i] == null) {
+                nombreArray[i] = nombreProducto;
+                i = nombreArray.length;
+                System.out.println("PRODUCTO: " + nombreProducto);
+            }
+        }
+
+    }
+
+
+}
